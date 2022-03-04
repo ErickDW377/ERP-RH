bddb8ce897bdfdb6f0257711f841b023ff5df3c0
Erick Diaz Wences <84026112+ErickDW377@users.noreply.github.com>
2022-03-03 23:29:54 -0600

    Consulta Puestos

--- controlador/DAO.py
+++ controlador/DAO.py
@@ -5,6 +5,36 @@ from sqlalchemy.sql.sqltypes import VARCHAR
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column,Integer,String,Date,Float,ForeignKey,Boolean
 from sqlalchemy.orm import relationship
+from flask_login import UserMixin
 
 
 db=SQLAlchemy()
+
+class Puestos(db.Model):
+    __tablename__= 'RH_Puestos'
+    idPuesto = Column(Integer, primary_key=True)
+    nombre = Column(String(60))
+    salarioMinimo = Column(Float)
+    salarioMaximo = Column(Float)
+    estatus = Column(String(1))
+
+    def registrar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def consultar(self,id):
+        return self.query.get(id)
+
+    def consultarAll(self):        
+        return self.query.all()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self,id):
+        objeto=self.consultar(id)
+        db.session.delete(objeto)
+        db.session.commit()
+
+
--- controlador/app-routing.py
+++ controlador/app-routing.py
@@ -3,14 +3,14 @@ from flask_bootstrap import Bootstrap
 from sqlalchemy.sql.elements import Null
 from sqlalchemy.sql.expression import select
 from sqlalchemy.sql.sqltypes import String
-from DAO import db
+from DAO import db, Puestos
 from flask_login import LoginManager,current_user,login_required,login_user,logout_user
 from array import array
 
 app=Flask(__name__,template_folder='../Pages',static_folder='../Static')
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Hola.123@127.0.0.1/checkCar'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Hola.123@127.0.0.1/recuhum'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='cl4v3'
 
@@ -23,8 +23,13 @@ app.secret_key='cl4v3'
 def iniciar():    
     return  render_template('Inicio/inicio.html')
 
+@app.route('/puestos')
+def puestos():
+    p=Puestos() 
+    puestos = p.consultarAll()   
+    return  render_template('Puestos/puestos.html', puestos = puestos)
 
-
-if __name__=='__main__': 
-    db.init_app(app)   
+if __name__=='__main__':
+    db.init_app(app)
     app.run(debug=True)
+    
